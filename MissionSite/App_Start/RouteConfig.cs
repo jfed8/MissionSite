ef15ae73ce76097d6a9556ec3fa9d3cfec3cee84
kyardley <klynt.yardley@gmail.com>
2016-10-27 22:49:27 -0600

    Created RouteConfig

--- MissionSite/App_Start/RouteConfig.cs
+++ MissionSite/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MissionSite
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Mission Details",
+                url: "{controller}/{action}/{missionName}"
+                
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- MissionSite/Controllers/MissionController.cs
+++ MissionSite/Controllers/MissionController.cs
@@ -17,7 +17,7 @@ namespace MissionSite.Controllers
 
                 switch (missionName)
                 {
-                    case "Bolivia, Cochabamba":
+                    case "Bolivia-Cochabamba":
                         ViewBag.President = "Mark W. Hansen";
                         ViewBag.Address = "Bolivia Cochabamba Mission\nCasilla de Correo 1375 \nCochabamba, Cochabamba \nBolivia 591-4-411-7207";
                         ViewBag.Language = "Spanish";
@@ -25,7 +25,7 @@ namespace MissionSite.Controllers
                         ViewBag.DominateReligion = "Catholic";
                         ViewBag.Flag = "../../img/bolivia.png";
                         break;
-                    case "Louisiana, Baton Rouge":
+                    case "Louisiana-Baton-Rouge":
                         ViewBag.President = "Reed H. Hansen";
                         ViewBag.Address = "Louisiana Baton Rouge Mission \n12025 Justice Ave \nBaton Rouge, LA 70816 \nUnited States";
                         ViewBag.Language = "English";
@@ -33,7 +33,7 @@ namespace MissionSite.Controllers
                         ViewBag.DominateReligion = "Catholic";
                         ViewBag.Flag = "../../img/louisiana.png";
                         break;
-                    case "Philippines, Tacloban":
+                    case "Philippines-Tacloban":
                         ViewBag.President = "Wayne Maurer";
                         ViewBag.Address = "Philippines Tacloban Mission \n6000 Maharlika Way \nFatima Village, Leyte \nPhilippines";
                         ViewBag.Language = "Cebauno & Waray Waray";
@@ -41,7 +41,7 @@ namespace MissionSite.Controllers
                         ViewBag.DominateReligion = "Catholic";
                         ViewBag.Flag = "../../img/philippinesFlag.png";
                         break;
-                    case "Colorado, Fort Collins":
+                    case "Colorado-Fort-Collins":
                         ViewBag.President = "Sean S. McMurray";
                         ViewBag.Address = "Colorado Fort Collins Mission \n500 Hillspire Dr \nWindsor, CO 80550 \nUnited States";
                         ViewBag.Language = "English";
@@ -49,6 +49,8 @@ namespace MissionSite.Controllers
                         ViewBag.DominateReligion = "Catholic";
                         ViewBag.Flag = "../../img/coloradoFlag.png";
                         break;
+                    default:
+                        return RedirectToAction("Missions", "Mission");
                 }
                 return View();
             }
