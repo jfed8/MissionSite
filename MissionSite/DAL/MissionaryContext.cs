a6dae72a4c094ea221321c75e807c868a1a6f59b
jfed8 <jadenfeddock@gmail.com>
2016-12-05 15:41:09 -0700

    DAL File

---
+++ MissionSite/DAL/MissionaryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MissionSite.Models;
+
+namespace MissionSite.DAL
+{
+    public class MissionaryContext : DbContext
+    {
+        public MissionaryContext() : base("MissionaryContext")
+        {
+
+        }
+
+        public DbSet<Missions> Missions { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<MissionQuestions> MissionQuestions { get; set; }
+
+    }
+}
